220d8eee5bf94481ebbbd29336666e932a08e300
Kaviya-1508 <kaviya.kavi15082006@gmail.com>
2026-07-09 15:59:16 +0530

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+export const config = { runtime: "edge" };
+import server from "../dist/server/server.js";
+
+export default async function handler(request: Request): Promise<Response> {
+  return server.fetch(request, {}, {});
+}
