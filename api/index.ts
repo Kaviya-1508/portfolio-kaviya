a731709984d4bc998c8268977e13af52b9eb1922
Kaviya-1508 <kaviya.kavi15082006@gmail.com>
2026-07-09 16:14:22 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
+// @ts-nocheck
+export const config = { runtime: "nodejs" };
+import server from "../dist/server/server.js";
+
+export default async function handler(request: Request): Promise<Response> {
+  return server.fetch(request, {}, {});
+}
 export const config = { runtime: "nodejs" };
 import server from "../dist/server/server.js";
 
